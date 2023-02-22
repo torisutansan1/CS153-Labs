5ec26c0ec338da2185b2088ca28b9f01cb697f8d
Joseph Chang <jchan443@sledge.cs.ucr.edu>
2023-02-22 14:17:56 -0800

    hey

--- proc.c
+++ proc.c
@@ -720,47 +720,49 @@ setpriority(int prior_val)
   return 0;
 }
 
-void
-lotteryscheduler(void)
-{
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
+// void
+// lotteryscheduler(void)
+// {
+//   struct proc *p;
+//   struct cpu *c = mycpu();
+//   c->proc = 0;
   
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-    // create a random ticket
-    int randTicket;
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      bool nTicket = false;
-      for(int i = 0; i < p->ticket.size(); i++){
-        if(p->ticket[i] == randTicket){
-          nTicket = true;
-        }
-      }
-      if(!nTicket)
-        continue;
-      if(p->state != RUNNABLE)
-        continue;
+//   for(;;){
+//     // Enable interrupts on this processor.
+//     sti();
+//     // create a random ticket
+//     int randTicket;
+//     // Loop over process table looking for process to run.
+//     acquire(&ptable.lock);
+//     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+//       bool nTicket = false;
+//       for(int i = 0; i < p->ticket.size(); i++){
+//         if(p->ticket[i] == randTicket){
+//           nTicket = true;
+//         }
+//       }
+//       if(!nTicket){
+//         continue;
+//         //randTicket; /*create a new rand ticket*/
+//       }
+//       if(p->state != RUNNABLE)
+//         continue;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+//       // Switch to chosen process.  It is the process's job
+//       // to release ptable.lock and then reacquire it
+//       // before jumping back to us.
+//       c->proc = p;
+//       switchuvm(p);
+//       p->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+//       swtch(&(c->scheduler), p->context);
+//       switchkvm();
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
+//       // Process is done running for now.
+//       // It should have changed its p->state before coming back.
+//       c->proc = 0;
+//     }
+//     release(&ptable.lock);
 
-  }
-}
+//   }
+// }
