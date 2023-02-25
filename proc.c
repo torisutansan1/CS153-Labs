0f5dbae2a2bf772babf3ae1fedae2a43d08944fe
Joseph Chang <jchan443@sledge.cs.ucr.edu>
2023-02-25 11:38:13 -0800

    fixed waiting time

--- proc.c
+++ proc.c
@@ -368,7 +368,7 @@ scheduler(void)
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
-      p->burstTime = ticks;
+      p->burstTime += 1;
 
       swtch(&(c->scheduler), p->context);
       switchkvm();
